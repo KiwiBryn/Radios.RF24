f78267f1bb2ee323f9a3e3baad5857bebfbffcbd
Josh Davis <josh@davis.fm>
2016-03-04 20:38:05 -0800

    Starting to tweak the test app

--- TestApp/App.xaml.cs
+++ TestApp/App.xaml.cs
@@ -1,4 +1,4 @@
-﻿namespace TestAppUWP
+﻿namespace TestApp
 {
     using System;
     using Windows.ApplicationModel;
--- TestApp/MainPage.xaml.cs
+++ TestApp/MainPage.xaml.cs
@@ -1,6 +1,6 @@
 ﻿// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
-namespace TestAppUWP
+namespace TestApp
 {
     using Radios.RF24;
     using System.Diagnostics;
@@ -19,73 +19,70 @@ namespace TestAppUWP
         private const byte IRQ_PIN = 4;
         private const byte CHANNEL = 10;
         private byte[] MY_ADDRESS = Encoding.UTF8.GetBytes("MYPI2");
-        private byte[] SEND_ADDRESS = Encoding.UTF8.GetBytes("MYPI1");
 
         private string[] MESSAGES = {"Message1", "TestMessage", "Another message", "this one is a really really really really long message compared to the others"};
 
-        RF24 radio;
+        public RF24 Radio;
         bool isInitialized = false;
 
         public MainPage()
         {
             this.InitializeComponent();
-            ButtonSend.Click += ButtonSend_Click;
+            SendButton.Click += ButtonSend_Click;
 
             Debug.WriteLine("THIS IS A TEST COMMENT");
-            radio = new RF24();
+            Radio = new RF24();
 
-            radio.OnDataReceived += Radio_OnDataReceived;
-            radio.OnTransmitFailed += Radio_OnTransmitFailed;
-            radio.OnTransmitSuccess += Radio_OnTransmitSuccess;
+            Radio.OnDataReceived += Radio_OnDataReceived;
+            Radio.OnTransmitFailed += Radio_OnTransmitFailed;
+            Radio.OnTransmitSuccess += Radio_OnTransmitSuccess;
 
-            radio.Initialize(CE_PIN, CS_LINE, IRQ_PIN);
-            radio.Address = MY_ADDRESS;
-            radio.Channel = CHANNEL;
+            Radio.Initialize(CE_PIN, CS_LINE, IRQ_PIN);
+            Radio.Address = MY_ADDRESS;
+            Radio.Channel = CHANNEL;
             
-            radio.IsEnabled = true;
-            var myAddress = radio.Address;
+            Radio.IsEnabled = true;
+            var myAddress = Radio.Address;
             Debug.WriteLine("I am " + new string(Encoding.UTF8.GetChars(myAddress)));
 
-            Debug.WriteLine("PA: " + radio.PowerLevel);
-            Debug.WriteLine("IsAutoAcknowledge: " + radio.IsAutoAcknowledge);
-            Debug.WriteLine("Channel: " + radio.Channel);
-            Debug.WriteLine("DataRate: " + radio.DataRate);
-            Debug.WriteLine("IsDynamicAcknowledge: " + radio.IsDyanmicAcknowledge);
-            Debug.WriteLine("IsDynamicPayload: " + radio.IsDynamicPayload);
-            Debug.WriteLine("IsEnabled: " + radio.IsEnabled);
-            Debug.WriteLine("Frequency: " + radio.Frequency);
-            Debug.WriteLine("IsInitialized: " + radio.IsInitialized);
-            Debug.WriteLine("IsPowered: " + radio.IsPowered);
+            Debug.WriteLine("PA: " + Radio.PowerLevel);
+            Debug.WriteLine("IsAutoAcknowledge: " + Radio.IsAutoAcknowledge);
+            Debug.WriteLine("Channel: " + Radio.Channel);
+            Debug.WriteLine("DataRate: " + Radio.DataRate);
+            Debug.WriteLine("IsDynamicAcknowledge: " + Radio.IsDyanmicAcknowledge);
+            Debug.WriteLine("IsDynamicPayload: " + Radio.IsDynamicPayload);
+            Debug.WriteLine("IsEnabled: " + Radio.IsEnabled);
+            Debug.WriteLine("Frequency: " + Radio.Frequency);
+            Debug.WriteLine("IsInitialized: " + Radio.IsInitialized);
+            Debug.WriteLine("IsPowered: " + Radio.IsPowered);
 
             isInitialized = true;
         }
 
         private void ButtonSend_Click(object sender, RoutedEventArgs e)
         {
-            //byte[] toSend = new byte[MessageSend.Text.Length + 1];
-            //Array.Copy(Encoding.UTF8.GetBytes(MessageSend.Text), toSend, MessageSend.Text.Length);
-            radio.SendTo(SEND_ADDRESS, Encoding.UTF8.GetBytes(MessageSend.Text));
+            Radio.SendTo(Encoding.UTF8.GetBytes(SendToAddress.Text), Encoding.UTF8.GetBytes(SendBuffer.Text));
         }
 
         private void Radio_OnTransmitSuccess()
         {
             Debug.WriteLine("Transmit Succeeded!");
             if (isInitialized)
-                Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () => MessageStatus.Text = "Transmit Succeeded");
+                Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () => SendStatus.Text = "Transmit Succeeded");
         }
 
         private void Radio_OnTransmitFailed()
         {
             Debug.WriteLine("Transmit FAILED");
             if (isInitialized)
-                Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () => MessageStatus.Text = "Transmit FAILED");
+                Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () => SendStatus.Text = "Transmit FAILED");
         }
 
         private void Radio_OnDataReceived(byte[] data)
         {
             Debug.WriteLine("Received: " + Encoding.UTF8.GetString(data));
             if (isInitialized)
-                Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () => MessageReceive.Text = Encoding.UTF8.GetString(data));
+                Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () => ReceiveBuffer.Text = Encoding.UTF8.GetString(data));
         }
     }
 }
